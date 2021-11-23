5470ffcb533b4f7645b1a322a8a7ca7349517c65
Jane <janechodance@gmail.com>
2021-11-22 21:25:41 -0500

    commit tree fb9555c0289ef32d99bc243a09de40a73d633729

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,12 @@
 class Post < ApplicationRecord
+    validates :title, presence: true
+    validates :content, length: {minimum: 250}
+    validates :summary, length: { maximum: 250}
+    validates :category,  inclusion: { in: ['Fiction', 'Non-Fiction'] }
+    validate :not_title
+    def not_title
+        if title === "True Facts"
+            errors.add(:title, "Not allowed")
+        end
+    end
 end
